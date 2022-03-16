d3407778586107d896ce99312a9469e951703ebf
liys666666 <401654546@qq.com>
2022-03-16 17:07:16 +0800

    成员变量---子类可用

--- switch/src/main/java/com/liys/lswitch/LSwitch.java
+++ switch/src/main/java/com/liys/lswitch/LSwitch.java
@@ -18,13 +18,13 @@ import android.util.AttributeSet;
  */
 public class LSwitch extends BaseSwitch{
 
-    private int offTextX = 3;
-    private int trackRadius; //背景 圆角
-    private int trackHeight; //背景 高度
+    protected int offTextX = 3;
+    protected int trackRadius; //背景 圆角
+    protected int trackHeight; //背景 高度
 
-    private int thumbRadius; //滑块 圆角
-    private int thumbHeight; //滑块 高度
-    private int thumbWidth; //滑块 宽度
+    protected int thumbRadius; //滑块 圆角
+    protected int thumbHeight; //滑块 高度
+    protected int thumbWidth; //滑块 宽度
 
     public LSwitch(Context context) {
         this(context, null);
@@ -39,7 +39,7 @@ public class LSwitch extends BaseSwitch{
         initAttr(attrs);
     }
 
-    private void initAttr(AttributeSet attrs){
+    protected void initAttr(AttributeSet attrs){
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.LSwitch);
         //获取自定义属性的值
         trackRadius = typedArray.getDimensionPixelOffset(R.styleable.LSwitch_track_radius, -1);
